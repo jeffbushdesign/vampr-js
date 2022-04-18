a58606b60e9da5c4694bc2d929d46a620df1c599
Jeff Bush <jeffbushdesign@gmail.com>
2022-04-18 19:21:11 +0000

    Add first round of methods

--- vampire.js
+++ vampire.js
@@ -10,22 +10,34 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
+    // (this = class Vampire, offspring is an array of Vampire, push the vampire that's been passed to the function)
+    this.offspring.push(vampire);
 
+    // set the vampire's parent as the class Vampire
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
-
+    let numberOfVampires = 0;
+    let currentVampire = this;
+    while (currentVampire.creator) {
+      currentVampire = currentVampire.creator;
+      numberOfVampires++;
+    }
+    return numberOfVampires;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    const seniorityOfThisVampire = this.numberOfVampiresFromOriginal;
+    const seniorityOfVampireInputted = vampire.numberOfVampiresFromOriginal;
+    return seniorityOfThisVampire < seniorityOfVampireInputted;
   }
 
   /** Stretch **/
